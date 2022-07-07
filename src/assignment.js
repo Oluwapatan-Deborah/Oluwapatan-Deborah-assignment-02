01fc45380b41b2c84b79116cb547c10a56e6e93a
Oluwapatan Deborah <105171956+Oluwapatan-Deborah@users.noreply.github.com>
2022-07-07 12:05:52 +0000

    attempt challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let total = 0;
+  for (let i =0;i<arrayOfNumbers.length; i++){
+    total += arrayOfNumbers[i];
+  }
+    return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumbers = [];
+  for (let numbers of arrayOfNumbers) {
+    if (numbers % 2 === 0){
+      evenNumbers.push(numbers);
+    }
+    
+  }
+    return evenNumbers.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
